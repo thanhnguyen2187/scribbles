1a72d2ab483fd5050d415ec5ca15a488cc029ee8
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-08-28 23:55:26 +0700

    Create the repository

---
+++ astro.config.mjs
@@ -0,0 +1,43 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Thanh's Scribbles",
+      social: {
+        github: "https://github.com/thanhnguyen2187/scribbles",
+      },
+      sidebar: [
+        {
+          label: "Miscellanies",
+          items: [
+            { label: "REPL-Driven Development", slug: "miscellanies/repl-driven-development" },
+          ],
+        },
+        {
+          label: "Software Design for Flexibility",
+          items: [
+            { slug: "sdf/100-index" },
+            {
+              label: "Chapter 1",
+              autogenerate: { directory: "sdf/chapter-1" },
+            },
+          ],
+        },
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+});
