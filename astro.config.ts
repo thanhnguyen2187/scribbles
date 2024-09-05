2c0ea81bcfce158dfb784c34a5086f848a8039b8
Thanh Nguyen <thanhnguyen2187@gmail.com>
2024-09-05 20:18:57 +0700

    Remove redundant folders

--- astro.config.ts
+++ astro.config.ts
@@ -30,17 +30,6 @@ export default defineConfig({
             },
           ],
         },
-        {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "guides/example" },
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
-        },
       ],
     }),
   ],
